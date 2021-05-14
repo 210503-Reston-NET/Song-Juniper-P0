942f2ca9a332cdb5c3dfc4ca529bf65f24686b6f
Juniper Song <songminseonk@gmail.com>
2021-05-13 22:17:44 -0700

    got rid of my sketchy file db

--- StoreApp/StoreUI/Entities/wssdbContext.cs
+++ StoreApp/StoreUI/Entities/wssdbContext.cs
@@ -24,15 +24,6 @@ namespace StoreUI.Entities
         public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<StoreFront> StoreFronts { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:p0-wss-server.database.windows.net,1433;Initial Catalog=wssdb;Persist Security Info=False;User ID=dbadmin;Password=Password!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
